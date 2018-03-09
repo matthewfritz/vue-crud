79ee85ab8c5cc146cb380d6e7548980e7dab31bf
Matthew Fritz <mattf@burbankparanormal.com>
2018-03-09 13:33:35 -0800

    Pushing new tasks now works

--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -6,19 +6,24 @@
 
 		<form>
 			<div class="row">
-				<div class="col-sm-12 form-group">
-					<input type="text"
-						class="form-control"
-						placeholder="Type something and press Enter..."
-						autofocus
-					/>
+				<div class="col-sm-12">
+					<ol v-if="tasks.length">
+						<TaskListItem v-bind="task" v-for="task in tasks" :key="task.id"></TaskListItem>
+					</ol>
+					<p class="text-center" v-else>
+						Start adding tasks by typing below...
+					</p>
 				</div>
 			</div>
 			<div class="row">
-				<div class="col-sm-12">
-					<ol>
-						<TaskListItem v-bind="task" v-for="task in tasks" :key="task.id"></TaskListItem>
-					</ol>
+				<div class="col-sm-12 form-group">
+					<input type="text"
+						class="form-control"
+						placeholder="Type something Homer can do instead and press Enter..."
+						autofocus
+						v-model="newTask"
+						@keydown.enter="addTask"
+					/>
 				</div>
 			</div>
 		</form>
@@ -34,18 +39,28 @@ export default {
     return {
 		title: 'What Can Homer Do Instead?',
 		newTask: '',
-		tasks: [
-			{
-				id: 1,
-				title: "Make a Vue CRUD application"
-			},
-			{
-				id: 2,
-				title: "Make yet another Vue CRUD application"
-			}
-		]
+		tasks: []
     }
   },
-  components: { TaskListItem }
+  components: { TaskListItem },
+  methods: {
+  	addTask: function() {
+  		if(this.newTask != '') {
+	  		let newId = this.taskCount() + 1;
+
+	  		// push the new task onto the array
+	  		this.tasks.push({
+	  			id: newId,
+	  			title: this.newTask
+	  		});
+
+	  		// clear out the newTask data property
+	  		this.newTask = '';
+  		}
+  	},
+	taskCount: function() {
+		return this.tasks.length;
+	}
+  }
 }
 </script>
